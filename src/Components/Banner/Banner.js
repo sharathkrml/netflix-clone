503692db8366f46d988120565079be52ed131071
Sharath V <sharathkrml@gmail.com>
2021-08-23 17:26:31 +0530

    Banner Updated with trending movie from tmdb

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import instance from '../../axios';
+import { API_KEY } from '../../constants/constants';
+import React,{useEffect, useState} from 'react'
 import './Banner.css';
 function Banner() {
+    const [bannermovie, setBannermovie] = useState()
+    useEffect(() => {
+        instance.get(`/trending/all/day?api_key=${API_KEY}`)
+        .then((response)=>{
+            setBannermovie(response.data.results[Math.floor((Math.random() * 20) + 1)])
+            })
+    }, [])
     return (
-        <div className='banner'>
+        <div style={{backgroundImage:`url(${bannermovie?'https://image.tmdb.org/t/p/w500'+bannermovie.backdrop_path:''})`}} className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie Name</h1>
+                <h1 className='title'>{bannermovie?bannermovie.title:''}</h1>
                 <div className='banner-buttons'>
                     <button className='play'>Play</button>
                     <button className='mylist'>My List</button>
                 </div>
-                <h1 className='description'>
-                     Irure occaecat id consequat exercitation labore cillum nostrud magna exercitation sint eu sunt esse voluptate. Ad velit laboris laboris labore anim enim mollit fugiat id mollit. Mollit enim et ullamco incididunt officia Lorem.
-                </h1>
+                <h1 className='description'>{bannermovie?bannermovie.overview:''}</h1>
             </div>
            {/* use div.fade-bottom */}
             <div className="fade-bottom"></div>
