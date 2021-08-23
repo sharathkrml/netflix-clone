7d5a9b5f2ab590cc995cc33b886d6fea16b5ce1b
Sharath V <sharathkrml@gmail.com>
2021-08-23 13:13:46 +0530

    added gradient below Banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -10,9 +10,11 @@ function Banner() {
                     <button className='mylist'>My List</button>
                 </div>
                 <h1 className='description'>
-                    Ipsum consequat enim velit irure eu ea id proident Lorem deserunt aliquip. Quis officia quis qui exercitation. Nisi nisi duis cillum nisi dolore. Irure occaecat id consequat exercitation labore cillum nostrud magna exercitation sint eu sunt esse voluptate. Ad velit laboris laboris labore anim enim mollit fugiat id mollit. Mollit enim et ullamco incididunt officia Lorem.
+                     Irure occaecat id consequat exercitation labore cillum nostrud magna exercitation sint eu sunt esse voluptate. Ad velit laboris laboris labore anim enim mollit fugiat id mollit. Mollit enim et ullamco incididunt officia Lorem.
                 </h1>
             </div>
+           {/* use div.fade-bottom */}
+            <div className="fade-bottom"></div>
         </div>
     )
 }
